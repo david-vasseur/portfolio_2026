0af0c6822d4d94481d78501e0a213766ec6622c8
David Vasseur <p5y4@macbook-pro-de-david.home>
2026-09-05 15:52:19 +0200

    clean code

--- components/pages/HomeSection.tsx
+++ components/pages/HomeSection.tsx
@@ -12,7 +12,7 @@ const HomeSection = () => {
                 2. lg:aspect-[16/10] pour verrouiller le ratio "Dashboard".
                 3. lg:max-h-[85vh] pour éviter que ça ne dépasse en haut/bas sur petits écrans.
             */}
-            <main className="z-10 grid w-full max-w-7xl h-full lg:h-auto lg:aspect-16/10 lg:max-h-[85vh] mx-auto backdrop-blur-lg rounded-4xl p-4 gap-4 grid-cols-1 grid-rows-[3fr_1fr_1fr] lg:grid-cols-3 lg:grid-rows-4">
+            <main className="z-10 min-h-0 grid w-full max-w-7xl h-full lg:h-auto lg:aspect-16/10 mx-auto backdrop-blur-lg rounded-4xl p-4 gap-4 grid-cols-1 grid-rows-[3fr_1fr_1fr] lg:grid-cols-3 lg:grid-rows-4">
                 
                 <HeroHome />
                 
